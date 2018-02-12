07ab05323d9d9377563d27ec57d70096d52e19e8
LikeJust <diddydwns135@gmail.com>
2018-02-12 20:40:37 +0900

    arr pointer ref

---
+++ Project6/arr_pointer_ref.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+/**
+ * \brief 
+ * \param size 
+ * \param arr1 
+ * \param arr2 
+ * \param arr3 
+ */
+void vector_add(unsigned size, double arr1[], double arr2[], double arr3[])
+{
+	for (unsigned i = 0; i < size; ++i)
+		arr3[i] = arr1[i] + arr2[i];
+}
+
+int main()
+{
+	double w[] = { 1.0, 2.0, 3.0 };
+
+	double x[3];
+
+	for(auto i=0; i<3; ++i)
+	{
+		x[i] = w[i] * 3;
+	}
+
+	auto y = new double[10];
+
+	vector_add(3, w, x, y);
+
+	delete[] y;
+
+	int i = 3;
+	int *ip = &i;
+
+	// 포인터 초기화
+	int *ip3 = nullptr;
+	int *ip4{};
+}
---
+++ Project6/smart_pointer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+	unique_ptr<double> dp{ new double };
+}
